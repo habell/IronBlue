6dd147ff599b5aae8a475307983a28ca877b67d6
Habel <tamikys15@gmail.com>
2022-09-19 22:39:55 +0300

    add tileMap, tilePallet and create new level with this options

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -21,13 +21,14 @@ public class test : MonoBehaviour
 
         Vector3 dir = Vector3.RotateTowards(_player.forward, _relativePos, 0.03f, 10f);
         Debug.DrawRay(_player.position, dir, Color.yellow);
+        Debug.DrawLine(_player.position, Enemy.position, Color.cyan);
         _rotation = Quaternion.LookRotation(dir);
         //_rotation = Quaternion.LookRotation(_relativePos);
         //Debug.DrawRay(_player.position, _relativePos, Color.magenta);
         //_rotation = Quaternion.Slerp(_player.rotation,_rotation, 0.05f);
         _player.rotation = _rotation;
         
-        _player.position = Vector3.MoveTowards(_player.position, Enemy.position, _speed);
+        _player.position = Vector3.MoveTowards(_player.position, Enemy.position, _speed);   
         
         //print(Quaternion.Angle(Enemy.rotation, _player.rotation));
     }
