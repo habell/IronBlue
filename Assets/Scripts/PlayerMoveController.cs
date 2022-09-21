60c6a8872a1cd2766c7259ed31fa6673615206f5
Habel <tamikys15@gmail.com>
2022-09-22 00:40:52 +0300

    add jumb base

--- Assets/Scripts/PlayerMoveController.cs
+++ Assets/Scripts/PlayerMoveController.cs
@@ -3,16 +3,20 @@ using UnityEngine;
 
 public class PlayerMoveController
 {
+    private float _maxJumpValue;
     private float _horizontalInput;
     private float _verticalInput;
     private Player _player;
     private Rigidbody2D _rigidbody2D;
+    private bool _jumped;
 
     public PlayerMoveController(Player player, Rigidbody2D rigidbody2D)
     {
         _player = player;
         _rigidbody2D = rigidbody2D;
         _rigidbody2D.freezeRotation = true;
+        _jumped = false;
+        _maxJumpValue = 10f;
     }
     
     public void Update()
@@ -23,6 +27,11 @@ public class PlayerMoveController
         if (_horizontalInput != 0)
         {
             _rigidbody2D.AddForce(Vector2.right * (_horizontalInput * 10));
+        }
+
+        if (_verticalInput > 0 & !_jumped)
+        {
+            
         }
     }
 }
