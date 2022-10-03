ecd86fb13410776d4672c77f0d18da20613adc5c
Habel <tamikys15@gmail.com>
2022-10-04 01:53:42 +0300

    create and add health script for player

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PlayerHealth : Health
+    {
+        public PlayerHealth(float healthValue) : base(healthValue)
+        {
+        }
+
+        protected override void Death()
+        {
+            Debug.Log("I am Die!");
+        }
+    }
+}
