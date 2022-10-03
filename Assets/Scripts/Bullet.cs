53776428f9d323f9efa23315107352439c8da3a2
Habel <tamikys15@gmail.com>
2022-10-03 16:49:56 +0300

    create bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,7 +1,29 @@
-﻿namespace DefaultNamespace
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
 {
-    public class Bullet
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Bullet : MonoBehaviour
     {
-        //TODO::
+        private Rigidbody2D _rigidbody2D;
+
+        [SerializeField]
+        private float _damage;
+
+        public Rigidbody2D RigidBody2D => _rigidbody2D;
+        private void Awake()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            if (TryGetComponent(out IHealth obj))
+            {
+                obj.Hit(_damage);
+            }
+            gameObject.SetActive(false);
+        }
     }
 }
