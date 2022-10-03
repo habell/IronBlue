eeb5196e049cf9babbf456abc6234305d6afa15b
Habel <tamikys15@gmail.com>
2022-10-04 00:37:58 +0300

    add new pickupable interface

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Interfaces;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour, IPickUpable
 {
     [SerializeField]
     private PlayerParameters _playerParameters;
