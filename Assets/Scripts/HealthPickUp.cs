6d8ad6afedcc527ada571fe421f009f373a8e6f0
Habel <tamikys15@gmail.com>
2022-10-04 01:49:38 +0300

    create HealthPickUp and his prefab

---
+++ Assets/Scripts/HealthPickUp.cs
@@ -0,0 +1,19 @@
+﻿using DefaultNamespace.Abstract;
+using Interfaces;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class HealthPickUp : PickUp
+    {
+        [SerializeField]
+        private float _healValue;
+
+        protected override void Pick(Collider2D pickuper)
+        {
+            if (!pickuper.gameObject.TryGetComponent(out Player player)) return;
+            player.Health.Heal(_healValue);
+            Destroy(gameObject);
+        }
+    }
+}
