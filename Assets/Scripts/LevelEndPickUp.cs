e1c17a3bcd8610e4635ec74b5400d7c3794afc25
Habel <tamikys15@gmail.com>
2022-10-04 01:51:35 +0300

    create level end script and his pickUp

---
+++ Assets/Scripts/LevelEndPickUp.cs
@@ -0,0 +1,14 @@
+﻿using DefaultNamespace.Abstract;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class LevelEndPickUp : PickUp
+    {
+        protected override void Pick(Collider2D pickuper)
+        {
+            if (!pickuper.gameObject.TryGetComponent(out Player player)) return;
+            Debug.Log("GameOver!");
+        }
+    }
+}
