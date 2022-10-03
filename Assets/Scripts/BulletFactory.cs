1bf965c5e613f5a7752669bed8cfae285e7083be
Habel <tamikys15@gmail.com>
2022-10-03 23:02:32 +0300

    bullet factory create (need prefab)

---
+++ Assets/Scripts/BulletFactory.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class BulletFactory
+    {
+        private Bullet _bullet;
+
+        public void SetFactoryBulletPrefab(Bullet bullet)
+        {
+            _bullet = bullet;
+        }
+
+        public Bullet Create()
+        {
+            return Object.Instantiate(_bullet);
+        }
+    }
+}
