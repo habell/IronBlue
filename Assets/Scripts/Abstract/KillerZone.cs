bdf472d596bd9e1579e1e74c3b79d659168a790a
Habel <tamikys15@gmail.com>
2022-10-04 19:07:18 +0300

    abstract killerzone

---
+++ Assets/Scripts/Abstract/KillerZone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Abstract
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    public abstract class KillerZone : MonoBehaviour
+    {
+        protected abstract void KillFunction(Player ply);
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.TryGetComponent(out Player ply))
+            {
+                KillFunction(ply);
+            }
+        }
+    }
+}
