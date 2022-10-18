755cdb39e7917729b4d3731b392900b999d2d706
Habel <tamikys15@gmail.com>
2022-10-18 23:55:28 +0300

    create requare, initial components, and add some func;

--- Assets/Scripts/Enemyes/FlyingWhale.cs
+++ Assets/Scripts/Enemyes/FlyingWhale.cs
@@ -1,22 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using DefaultNamespace;
 using Interfaces;
 using Pathfinding;
+using Unity.VisualScripting;
 using UnityEngine;
 
-namespace DefaultNamespace
+namespace Enemyes
 {
-    [RequireComponent(typeof(BoxCollider2D))]
+    [RequireComponent(typeof(BoxCollider2D), typeof(AIDestinationSetter),
+        typeof(Seeker))]
+    [RequireComponent(typeof(AIPath), typeof(Rigidbody2D))]
     public class FlyingWhale : MonoBehaviour, IEnemy, IPatrollable
     {
+        private const float MIN_DISTANCE_TARGET = 1f;
         private AIDestinationSetter _aiDestinationSetter;
 
         private int _targetID;
 
         [SerializeField]
         private List<Transform> _positions;
+
         private void Awake()
         {
+            GetComponent<BoxCollider2D>().isTrigger = true;
+            GetComponent<Rigidbody2D>().isKinematic = true;
+
+            _aiDestinationSetter = GetComponent<AIDestinationSetter>();
             _targetID = 0;
             _aiDestinationSetter.target = _positions[_targetID];
         }
@@ -33,23 +44,45 @@ namespace DefaultNamespace
         {
             if (other.TryGetComponent(out Player ply))
             {
-                print("it is ply");
+                NextPatrolPosition();
             }
+        }
+
+        private void FixedUpdate()
+        {
+            if (Vector2.Distance(transform.position, _positions[_targetID].position) < MIN_DISTANCE_TARGET)
+            {
+                NextPatrolPosition();
+            }
+        }
+
+        public void StartPatrol()
+        {
+            throw new System.NotImplementedException();
         }
 
         public void StartHunt(Transform victim)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
-        void IPatrollable.StopHunt()
+        public void StopHunt()
         {
             throw new System.NotImplementedException();
         }
 
-        void IPatrollable.StartPatrol()
+        public void NextPatrolPosition()
         {
-            throw new System.NotImplementedException();
+            if (_targetID > 0 && _targetID % _positions.Count-1 == 0)
+            {
+                _targetID = 0;
+            }
+            else
+            {
+                _targetID++;
+            }
+
+            _aiDestinationSetter.target = _positions[_targetID];
         }
     }
 }
