36a23c281d1bc17a824ff516a865b8c215cc25a5
Habel <tamikys15@gmail.com>
2022-10-18 23:16:15 +0300

    move cannon

---
+++ Assets/Scripts/Enemyes/MuzzleCannon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using DefaultNamespace;
+using UnityEngine;
+
+public class MuzzleCannon
+{
+    private Transform _muzzle;
+
+    private Transform _aim;
+
+    private readonly Cannon _cannon;
+
+    public MuzzleCannon(Cannon cannon)
+    {
+        _cannon = cannon;
+        _muzzle = _cannon.Muzzle;
+    }
+
+    public void SetTarget(Transform target)
+    {
+        _aim = target;
+    }
+
+    public void FixedUpdate()
+    {
+        if (!_aim) return;
+        var dir = _muzzle.position - _aim.position;
+        _muzzle.rotation =
+            Quaternion.AngleAxis(Vector2.Angle(Vector2.down, dir), Vector3.Cross(Vector2.down, dir));
+    }
+}
