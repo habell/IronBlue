3fc30e397a85cbfa4a389e8a50228b8d7a8202c4
Habel <tamikys15@gmail.com>
2022-10-24 00:03:32 +0300

    add level generatior

---
+++ Assets/Scripts/LevelGenerator/LevelGeneratorView.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace LevelGenerator
+{
+    public class LevelGeneratorView : MonoBehaviour
+    {
+        [SerializeField]
+        private Tilemap _tileMapGround;
+
+        [SerializeField]
+        private Tile _tileMap;
+
+        [SerializeField]
+        private int _wight;
+
+        [SerializeField]
+        private int _height;
+
+        [SerializeField]
+        private int _factorSmooth;
+
+        [SerializeField, Range(0, 100)]
+        private int _randomProcent;
+
+
+        public Tilemap TileMapGround => _tileMapGround;
+
+        public Tile TileMap => _tileMap;
+
+        public int Wight => _wight;
+
+        public int Height => _height;
+
+        public int FactorSmooth => _factorSmooth;
+
+        public int RandomProcent => _randomProcent;
+    }
+}
