b275bdfe26ec195cf4afee263ba1cba47a4590e1
Habel <tamikys15@gmail.com>
2022-10-25 17:03:59 +0300

    refactor controller and add monobeh

--- Assets/Scripts/LevelGenerator/LevelGeneratorController.cs
+++ Assets/Scripts/LevelGenerator/LevelGeneratorController.cs
@@ -5,47 +5,20 @@ using UnityEngine;
 
 namespace LevelGenerator
 {
-    public class LevelGeneratorController
+    public class LevelGeneratorController : MonoBehaviour
     {
-        private const int WALLS = 4;
-
-        private int[,] _map;
-
+        private LevelGeneratorModel _model;
         private LevelGeneratorView _view;
 
-        public LevelGeneratorController(LevelGeneratorView view)
-        {
-            _view = view;
-        }
+        public LevelGeneratorView View => _view;
 
-        public void Awake()
-        {
-            GenerateLevel();
-        }
+        public LevelGeneratorModel Model => _model;
 
-        private void GenerateLevel()
+        private void Awake()
         {
-            RandomFillMap();
-        }
-
-        private void RandomFillMap()
-        {
-            var random = new System.Random();
-            for (int x = 0; x < _view.Height; x++)
-            {
-                for (int y = 0; y < _view.Wight; y++)
-                {
-                    if (x == 0 || x >= _view.Height - 1 || y == 0 || y >= _view.Wight - 1)
-                    {
-                        _map[x, y] = 1;
-                    }
-                    else
-                    {
-                        _map[x, y] = random.Next(0, 100) < _view.MapFillProcent ? 1 : 0;
-                    }
-                }
-            }
-            Debug.Log(_map);
+            _view = GetComponent<LevelGeneratorView>();
+            _model = new LevelGeneratorModel(_view);
+            _model.Awake();
         }
     }
 }
