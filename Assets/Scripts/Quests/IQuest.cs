7a9c769fb8aff0be4020d363a6d0ccc9caf7aae4
Habel <tamikys15@gmail.com>
2022-11-02 22:19:18 +0300

    Write contract

---
+++ Assets/Scripts/Quests/IQuest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Quests
+{
+    public interface IQuest
+    {
+        event Action<IQuest> Completed;
+        bool IsCompleted { get; }
+        public void ResetQuest();
+    }
+}
