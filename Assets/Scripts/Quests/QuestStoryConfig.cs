e4557208b0f3cddfd6a43a1b4326a3cd64b825ca
Habel <tamikys15@gmail.com>
2022-11-02 21:00:26 +0300

    Quest story system

---
+++ Assets/Scripts/Quests/QuestStoryConfig.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "QuestStoryConfig", menuName = "Quest system/Story Configuration")]
+public class QuestStoryConfig : ScriptableObject
+{
+    [SerializeField]
+    private QuestConfig[] _quests;
+
+    [SerializeField]
+    private QuestStoryType _storyType;
+
+    public QuestConfig[] Quests => _quests;
+
+    public QuestStoryType StoryType => _storyType;
+}
+
+public enum QuestStoryType
+{
+    Common,
+    Reusable,
+}
