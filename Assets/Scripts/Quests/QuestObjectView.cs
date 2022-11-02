c5fcee4b4d420bda708e528624adbc986a87221b
Habel <tamikys15@gmail.com>
2022-11-02 22:21:30 +0300

    Initial write

---
+++ Assets/Scripts/Quests/QuestObjectView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace Quests
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    public class QuestObjectView : MonoBehaviour
+    {
+        [SerializeField]
+        private Color _completeColor;
+
+        [SerializeField]
+        private int _id;
+        
+        private Color _defaultColor;
+
+        public int ID => _id;
+
+        public Action<Player> ObjectContact;
+
+        private void Awake()
+        {
+            _defaultColor = GetComponent<SpriteRenderer>().color;
+        }
+        
+
+        public void ProcessComplete()
+        {
+            
+        }
+
+        public void ProcessActivate()
+        {
+            
+        }
+    }
+}
