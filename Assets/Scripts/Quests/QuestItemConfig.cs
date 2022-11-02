7631bd2e5c8b8099000ff54953eeee078efa147d
Habel <tamikys15@gmail.com>
2022-11-02 20:59:57 +0300

    Create quest config parameters

---
+++ Assets/Scripts/Quests/QuestItemConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "QuestItemConfig", menuName = "Quest system/Quest Item Config")]
+public class QuestItemConfig : ScriptableObject
+{
+    [SerializeField]
+    private int _questID;
+
+    [SerializeField]
+    private List<int> _questItemIDCollection;
+
+    public List<int> QuestItemIDCollection => _questItemIDCollection;
+
+    public int QuestID => _questID;
+}
