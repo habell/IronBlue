675ba2a4642e519d0ad94efdc6c7988c16ed46e2
Habel <tamikys15@gmail.com>
2022-11-03 19:27:15 +0300

    Quest configurator init.

---
+++ Assets/Scripts/Quests/QuestConfigurator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Quests
+{
+    public class QuestConfigurator : MonoBehaviour
+    {
+        [SerializeField]
+        private QuestObjectView _sigleQuestView;
+
+        private Quest _quest;
+
+        private void Awake()
+        {
+            _quest = new Quest(_sigleQuestView, new SwitchQuestModel());
+        }
+
+        private void OnDestroy()
+        {
+            _quest.Dispose();
+        }
+    }
+}
